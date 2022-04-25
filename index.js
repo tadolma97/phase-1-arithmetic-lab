92819450449f66efa2b438c77a949a5d7aa3f12c
Tashi Dolma <tadolma97@gmail.com>
2022-04-25 16:23:25 -0400

    commit tree 0b8bd4ded56de0876b741549e273984d87b38bfd

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply=num1*num2
+
+
+const random=Math.ceil(Math.random()*10)+1;
+
+const num3= 54
+const num4= 5
+const mod=num3%num4
+
+const max=Math.max(5,7,12,17,20);
